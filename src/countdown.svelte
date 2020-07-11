7335930905e5dde84f22425aad5f53d1fb6e7bf9
xuezzou <xuez@lyft.com>
2020-07-10 20:11:31 -0500

    update countdown

--- src/countdown.svelte
+++ src/countdown.svelte
@@ -8,14 +8,11 @@ setInterval(() => {now = new Date().getTime();}, 1000)
 $: distance = countDownDate - now;
 
 // Time calculations for days, hours, minutes and seconds
-$: days = Math.floor(distance / (1000 * 60 * 60 * 24));
-$: hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-$: minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-$: seconds = Math.floor((distance % (1000 * 60)) / 1000);
+$: days = distance >= 0 ? Math.floor(distance / (1000 * 60 * 60 * 24)) : 0;
+$: hours =  distance >= 0 ? Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)): 0;
+$: minutes = distance >= 0 ?  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)): 0;
+$: seconds = distance >= 0 ? Math.floor((distance % (1000 * 60)) / 1000): 0;
 
-if (distance < 0) {
-
-}
 </script>
 
 <style>
